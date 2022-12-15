9d969d370af9f225188ac9ab96d561215ecbc8b7
JeanPCordeiro <62947268+JeanPCordeiro@users.noreply.github.com>
2022-12-15 17:00:23 +0100

    Update _get_kubeconfig.sh

--- _get_kubeconfig.sh
+++ _get_kubeconfig.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -o xtrace
 
-sudo scp root@vmi1053342.contaboserver.net:/etc/rancher/k3s/k3s.yaml /etc/.
-sudo sed -i 's/127.0.0.1/vmi1053342.contaboserver.net/g' /etc/k3s.yaml
+export MASTER=vmi1053342.contaboserver.net
+
+#
+# Install Kubectl & Helm
+#
+curl -LO https://dl.k8s.io/release/v1.23.0/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+brew install helm
+
+#
+# Get KUBECONGIG from MASTER
+#
+sudo scp root@${MASTER}:/etc/rancher/k3s/k3s.yaml /etc/.
+sudo sed -i 's/127.0.0.1/${MASTER}/g' /etc/k3s.yaml
 sudo chmod +r /etc/k3s.yaml
 export KUBECONFIG=/etc/k3s.yaml
