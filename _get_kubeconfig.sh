77afb61d1086dfd344ec94538123719c9277f0ed
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2022-12-15 16:55:32 +0000

    remove .gitpod

--- _get_kubeconfig.sh
+++ _get_kubeconfig.sh
@@ -8,12 +8,19 @@ export MASTER=vmi1053342.contaboserver.net
 #
 curl -LO https://dl.k8s.io/release/v1.23.0/bin/linux/amd64/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo rm kubectl
 brew install helm
+
+sudo apt update
+sudo apt-get install gettext-base -y
 
 #
 # Get KUBECONGIG from MASTER
 #
-sudo scp root@${MASTER}:/etc/rancher/k3s/k3s.yaml /etc/.
-sudo sed -i 's/127.0.0.1/${MASTER}/g' /etc/k3s.yaml
+scp root@${MASTER}:/etc/rancher/k3s/k3s.yaml _k3s.yaml
+sudo sed -i 's/127.0.0.1/${MASTER}/g' _k3s.yaml
+cat _k3s.yaml | envsubst > k3s.yaml
+rm _k3s.yaml
+sudo mv k3s.yaml /etc/k3s.yaml
 sudo chmod +r /etc/k3s.yaml
 export KUBECONFIG=/etc/k3s.yaml
