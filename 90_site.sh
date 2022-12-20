12061881dcdc720ea2653eb57e3e0ece36c7fc0b
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2022-12-20 17:22:48 +0000

    web site

--- 90_site.sh
+++ 90_site.sh
@@ -3,14 +3,15 @@ set -o xtrace
 
 export KUBECONFIG=/etc/k3s.yaml
 
-export NAMESITE=site2
+export NAMESITE=site
 
 export EMAIL=jeanpierre.cordeiro@gmail.com
-export DOMAIN=${NAMESITE}.lean-sys.com
+#export DOMAIN=${NAMESITE}.lean-sys.com
+export DOMAIN=lean-sys.com
 
 #
 # Install Wordpress
 #
 kubectl create ns ${NAMESITE}
-cat wordpress-local.yaml | envsubst | kubectl apply -f - -n ${NAMESITE}
-#cat wordpress.yaml | envsubst | kubectl apply -f - -n ${NAMESITE}
+#cat wordpress-local.yaml | envsubst | kubectl apply -f - -n ${NAMESITE}
+cat wordpress.yaml | envsubst | kubectl apply -f - -n ${NAMESITE}
