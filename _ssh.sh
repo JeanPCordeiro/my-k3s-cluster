dada8e37accea863592f7e98cfb299d97e90b181
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2022-12-30 12:47:35 +0000

    Setup with Ansible

--- _ssh.sh
+++ _ssh.sh
@@ -6,17 +6,13 @@ export MASTER2=vmi1026786.contaboserver.net
 export MASTER3=vmi1026787.contaboserver.net
 
 #
-# Install Kubectl & Helm
+# Set PasswordLess
 #
 
 rm ~/.ssh/known_hosts
 
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 
-scp ~/.ssh/id_rsa.pub root@${MASTER1}:./
-scp ~/.ssh/id_rsa.pub root@${MASTER2}:./
-scp ~/.ssh/id_rsa.pub root@${MASTER3}:./
-
-ssh root@${MASTER1} "mkdir .ssh ; cp id_rsa.pub .ssh/authorized_keys"
-ssh root@${MASTER2} "mkdir .ssh ; cp id_rsa.pub .ssh/authorized_keys"
-ssh root@${MASTER3} "mkdir .ssh ; cp id_rsa.pub .ssh/authorized_keys"
+ssh-copy-id root@${MASTER1}
+ssh-copy-id root@${MASTER2}
+ssh-copy-id root@${MASTER3}
